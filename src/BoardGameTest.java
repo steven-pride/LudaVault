fa107f685dd9f045fdb95aa3893fd07d669773f1
Steven Pride <spride5@mail.valenciacollege.edu>
2025-10-15 00:46:24 -0400

    Implemented tests for BoardGame

--- src/BoardGameTest.java
+++ src/BoardGameTest.java
@@ -4,16 +4,23 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardGameTest {
+    BoardGame boardGame;
 
     @BeforeEach
     void setUp() {
+        boardGame = new BoardGame(1, "Azul", 4, 60, 2.2, false);
     }
 
     @Test
     void calculateTRS() {
+        double trs = boardGame.calculateTRS();
+        assertEquals(2.2, trs);
     }
 
     @Test
     void testToString() {
+        String expected = "BoardGame [gameId=1, title=Azul, maxPlayers=4, playTime=60, weight=2.2, isExpansion=false]";
+        String actual = boardGame.toString();
+        assertEquals(expected, actual);
     }
 }
